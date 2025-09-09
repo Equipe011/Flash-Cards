a5e22af597df9569418495458b31e7c2687b21cc
Equipe011 <thomas.krutkevitz@escola.pr.gov.br>
2025-09-09 00:13:02 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Física',
+    'O que é a Lei da Inércia?',
+    'A Lei da Inércia afirma que um corpo tende a permanecer em repouso ou em movimento uniforme, a menos que uma força atue sobre ele.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é o clima equatorial?',
+    'É um tipo de clima quente e úmido, com chuvas abundantes durante o ano todo, típico de regiões próximas à Linha do Equador.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Química',
+    'O que é uma reação de neutralização?',
+    'É uma reação entre um ácido e uma base, que resulta na formação de sal e água.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'Em um triângulo retângulo, o quadrado da hipotenusa é igual à soma dos quadrados dos catetos.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'Foi um movimento social e político que derrubou a monarquia na França em 1789 e instaurou uma república baseada em ideias iluministas.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é um verbo?',
+    'É a classe de palavras que indica ação, estado, mudança de estado ou fenômeno da natureza.'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz "obrigado" em inglês?',
+    'A palavra "obrigado" em inglês é "thank you".'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é cultura?',
+    'Cultura é o conjunto de conhecimentos, crenças, arte, moral, leis e costumes de um grupo social.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é mitose?',
+    'Mitose é o processo de divisão celular que resulta em duas células-filhas com o mesmo número de cromossomos da célula-mãe.'
 )
